21d1ce98c9772cb4a19c538a8b0612fd1340fda7
Kirill Kramarenko <iamrfx@gmail.com>
2023-02-26 19:17:01 +0300

    Added initial currency exchange rates service Spring Boot project body after Spring Initializr

---
+++ currency-exchange-rates-service/src/main/java/com/roofflex/currencyexchangeratesservice/CurrencyExchangeRatesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.roofflex.currencyexchangeratesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeRatesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeRatesServiceApplication.class, args);
+	}
+
+}
---
+++ currency-exchange-rates-service/src/test/java/com/roofflex/currencyexchangeratesservice/CurrencyExchangeRatesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roofflex.currencyexchangeratesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeRatesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
